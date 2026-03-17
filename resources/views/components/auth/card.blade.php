8eedeeceb4915953c3381324006bc6c8e335ca7e
olvikt <39388293+olvikt@users.noreply.github.com>
2026-03-17 23:51:36 +0200

    Fix auth card full width in auth screens

--- resources/views/components/auth/card.blade.php
+++ resources/views/components/auth/card.blade.php
@@ -1,3 +1,3 @@
-<div class="bg-white/5 backdrop-blur rounded-2xl p-6 space-y-4 shadow-xl">
+<div class="w-full bg-white/5 backdrop-blur rounded-2xl p-6 space-y-4 shadow-xl">
     {{ $slot }}
 </div>
