8f7b37ce51721cbebd2d884ef5094f90df989e86
olvikt <39388293+olvikt@users.noreply.github.com>
2026-05-09 13:14:20 +0300

    Fix trial gift icon size and subscription modal mobile padding

--- resources/views/components/poof/modal.blade.php
+++ resources/views/components/poof/modal.blade.php
@@ -2,6 +2,7 @@
     'maxWidth' => 'max-w-sm',
     'overlayClass' => '',
     'panelClass' => '',
+    'overlayPaddingClass' => 'px-4',
 ])
 
 @php
@@ -13,7 +14,7 @@
     x-show="$wire.{{ $model }}"
     x-transition.opacity
     x-cloak
-    class="fixed inset-0 z-50 flex items-center justify-center bg-black/60 px-4 {{ $overlayClass }}"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black/60 {{ $overlayPaddingClass }} {{ $overlayClass }}"
 >
     <div
         @click.outside="$wire.set('{{ $model }}', false)"
